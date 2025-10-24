0dab70bed049b3bab5f809ce7a6e56b3ce9e99e3
itishhanda <itish.handa@gmail.com>
2025-10-24 11:06:21 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,14 +34,13 @@ const stampedVisaCountries = [
 // Special countries with different conditions for transit visas only
 const specialCountries = ['HT', 'CU'];
 
-// Countries eligible for transit exemptions (can transit without visa if they meet conditions)
-const transitExemptionCountries = [
+// Countries that must meet transit requirements (your provided list)
+const transitRequirementCountries = [
     'AF', 'AO', 'DZ', 'AZ', 'BD', 'BJ', 'BF', 'BI', 'CV', 'CM',
     'TD', 'CG', 'CD', 'CI', 'DJ', 'EG', 'GQ', 'ER', 'ET', 'GA',
-    'GM', 'GE', 'GH', 'GN', 'GW', 'KZ', 'KE', 'KG', 'LR',
-    'ML', 'MR', 'MZ', 'NP', 'NE', 'NG', 'PK', 'SN', 'SL', 'SO',
-    'LK', 'SD', 'TJ', 'TG', 'TM', 'UZ'
-    // India removed from transit exemption countries
+    'GM', 'GE', 'GH', 'GN', 'GW', 'KZ', 'KE', 'KG', 'LR', 'ML',
+    'MR', 'MZ', 'NP', 'NE', 'NG', 'PK', 'SN', 'SL', 'SO', 'LK',
+    'SD', 'TJ', 'TG', 'TM', 'UZ', 'IN' // Added India
 ];
 
 // Privileged visa countries
@@ -54,7 +53,7 @@ const euCountries = [
 ];
 privilegedVisaCountries.push(...euCountries);
 
-// All countries for the dropdown (Barbados removed)
+// All countries for the dropdown
 const allCountries = [
     {code: 'AF', name: 'Afghanistan'}, {code: 'AL', name: 'Albania'}, {code: 'DZ', name: 'Algeria'}, {code: 'AD', name: 'Andorra'}, {code: 'AO', name: 'Angola'}, {code: 'AG', name: 'Antigua and Barbuda'}, {code: 'AR', name: 'Argentina'}, {code: 'AM', name: 'Armenia'}, {code: 'AU', name: 'Australia'}, {code: 'AT', name: 'Austria'}, {code: 'AZ', name: 'Azerbaijan'}, {code: 'BS', name: 'Bahamas'}, {code: 'BH', name: 'Bahrain'}, {code: 'BD', name: 'Bangladesh'}, {code: 'BY', name: 'Belarus'}, {code: 'BE', name: 'Belgium'}, {code: 'BZ', name: 'Belize'}, {code: 'BJ', name: 'Benin'}, {code: 'BT', name: 'Bhutan'}, {code: 'BO', name: 'Bolivia'}, {code: 'BA', name: 'Bosnia and Herzegovina'}, {code: 'BW', name: 'Botswana'}, {code: 'BR', name: 'Brazil'}, {code: 'BN', name: 'Brunei'}, {code: 'BG', name: 'Bulgaria'}, {code: 'BF', name: 'Burkina Faso'}, {code: 'BI', name: 'Burundi'}, {code: 'CV', name: 'Cabo Verde'}, {code: 'KH', name: 'Cambodia'}, {code: 'CM', name: 'Cameroon'}, {code: 'CA', name: 'Canada'}, {code: 'CF', name: 'Central African Republic'}, {code: 'TD', name: 'Chad'}, {code: 'CL', name: 'Chile'}, {code: 'CN', name: 'China'}, {code: 'CO', name: 'Colombia'}, {code: 'KM', name: 'Comoros'}, {code: 'CG', name: 'Congo'}, {code: 'CD', name: 'Democratic Republic of Congo'}, {code: 'CR', name: 'Costa Rica'}, {code: 'CI', name: 'Ivory Coast'}, {code: 'HR', name: 'Croatia'}, {code: 'CU', name: 'Cuba'}, {code: 'CY', name: 'Cyprus'}, {code: 'CZ', name: 'Czech Republic'}, {code: 'DK', name: 'Denmark'}, {code: 'DJ', name: 'Djibouti'}, {code: 'DM', name: 'Dominica'}, {code: 'DO', name: 'Dominican Republic'}, {code: 'EC', name: 'Ecuador'}, {code: 'EG', name: 'Egypt'}, {code: 'SV', name: 'El Salvador'}, {code: 'GQ', name: 'Equatorial Guinea'}, {code: 'ER', name: 'Eritrea'}, {code: 'EE', name: 'Estonia'}, {code: 'SZ', name: 'Eswatini'}, {code: 'ET', name: 'Ethiopia'}, {code: 'FJ', name: 'Fiji'}, {code: 'FI', name: 'Finland'}, {code: 'FR', name: 'France'}, {code: 'GA', name: 'Gabon'}, {code: 'GM', name: 'Gambia'}, {code: 'GE', name: 'Georgia'}, {code: 'DE', name: 'Germany'}, {code: 'GH', name: 'Ghana'}, {code: 'GR', name: 'Greece'}, {code: 'GD', name: 'Grenada'}, {code: 'GT', name: 'Guatemala'}, {code: 'GN', name: 'Guinea'}, {code: 'GW', name: 'Guinea-Bissau'}, {code: 'GY', name: 'Guyana'}, {code: 'HT', name: 'Haiti'}, {code: 'HN', name: 'Honduras'}, {code: 'HU', name: 'Hungary'}, {code: 'IS', name: 'Iceland'}, {code: 'IN', name: 'India'}, {code: 'ID', name: 'Indonesia'}, {code: 'IR', name: 'Iran'}, {code: 'IQ', name: 'Iraq'}, {code: 'IE', name: 'Ireland'}, {code: 'IL', name: 'Israel'}, {code: 'IT', name: 'Italy'}, {code: 'JM', name: 'Jamaica'}, {code: 'JP', name: 'Japan'}, {code: 'JO', name: 'Jordan'}, {code: 'KZ', name: 'Kazakhstan'}, {code: 'KE', name: 'Kenya'}, {code: 'KI', name: 'Kiribati'}, {code: 'XK', name: 'Kosovo'}, {code: 'KW', name: 'Kuwait'}, {code: 'KG', name: 'Kyrgyzstan'}, {code: 'LA', name: 'Laos'}, {code: 'LV', name: 'Latvia'}, {code: 'LB', name: 'Lebanon'}, {code: 'LS', name: 'Lesotho'}, {code: 'LR', name: 'Liberia'}, {code: 'LY', name: 'Libya'}, {code: 'LI', name: 'Liechtenstein'}, {code: 'LT', name: 'Lithuania'}, {code: 'LU', name: 'Luxembourg'}, {code: 'MG', name: 'Madagascar'}, {code: 'MW', name: 'Malawi'}, {code: 'MY', name: 'Malaysia'}, {code: 'MV', name: 'Maldives'}, {code: 'ML', name: 'Mali'}, {code: 'MT', name: 'Malta'}, {code: 'MH', name: 'Marshall Islands'}, {code: 'MR', name: 'Mauritania'}, {code: 'MU', name: 'Mauritius'}, {code: 'MX', name: 'Mexico'}, {code: 'FM', name: 'Micronesia'}, {code: 'MD', name: 'Moldova'}, {code: 'MC', name: 'Monaco'}, {code: 'MN', name: 'Mongolia'}, {code: 'ME', name: 'Montenegro'}, {code: 'MA', name: 'Morocco'}, {code: 'MZ', name: 'Mozambique'}, {code: 'MM', name: 'Myanmar'}, {code: 'NA', name: 'Namibia'}, {code: 'NR', name: 'Nauru'}, {code: 'NP', name: 'Nepal'}, {code: 'NL', name: 'Netherlands'}, {code: 'NZ', name: 'New Zealand'}, {code: 'NI', name: 'Nicaragua'}, {code: 'NE', name: 'Niger'}, {code: 'NG', name: 'Nigeria'}, {code: 'KP', name: 'North Korea'}, {code: 'MK', name: 'North Macedonia'}, {code: 'NO', name: 'Norway'}, {code: 'OM', name: 'Oman'}, {code: 'PK', name: 'Pakistan'}, {code: 'PW', name: 'Palau'}, {code: 'PS', name: 'Palestine'}, {code: 'PA', name: 'Panama'}, {code: 'PG', name: 'Papua New Guinea'}, {code: 'PY', name: 'Paraguay'}, {code: 'PE', name: 'Peru'}, {code: 'PH', name: 'Philippines'}, {code: 'PL', name: 'Poland'}, {code: 'PT', name: 'Portugal'}, {code: 'QA', name: 'Qatar'}, {code: 'RO', name: 'Romania'}, {code: 'RU', name: 'Russia'}, {code: 'RW', name: 'Rwanda'}, {code: 'KN', name: 'Saint Kitts and Nevis'}, {code: 'LC', name: 'Saint Lucia'}, {code: 'VC', name: 'Saint Vincent and the Grenadines'}, {code: 'WS', name: 'Samoa'}, {code: 'SM', name: 'San Marino'}, {code: 'ST', name: 'Sao Tome and Principe'}, {code: 'SA', name: 'Saudi Arabia'}, {code: 'SN', name: 'Senegal'}, {code: 'RS', name: 'Serbia'}, {code: 'SC', name: 'Seychelles'}, {code: 'SL', name: 'Sierra Leone'}, {code: 'SG', name: 'Singapore'}, {code: 'SK', name: 'Slovakia'}, {code: 'SI', name: 'Slovenia'}, {code: 'SB', name: 'Solomon Islands'}, {code: 'SO', name: 'Somalia'}, {code: 'ZA', name: 'South Africa'}, {code: 'KR', name: 'South Korea'}, {code: 'SS', name: 'South Sudan'}, {code: 'ES', name: 'Spain'}, {code: 'LK', name: 'Sri Lanka'}, {code: 'SD', name: 'Sudan'}, {code: 'SR', name: 'Suriname'}, {code: 'SE', name: 'Sweden'}, {code: 'CH', name: 'Switzerland'}, {code: 'SY', name: 'Syria'}, {code: 'TW', name: 'Taiwan'}, {code: 'TJ', name: 'Tajikistan'}, {code: 'TZ', name: 'Tanzania'}, {code: 'TH', name: 'Thailand'}, {code: 'TG', name: 'Togo'}, {code: 'TO', name: 'Tonga'}, {code: 'TT', name: 'Trinidad and Tobago'}, {code: 'TN', name: 'Tunisia'}, {code: 'TR', name: 'Turkey'}, {code: 'TM', name: 'Turkmenistan'}, {code: 'TV', name: 'Tuvalu'}, {code: 'UG', name: 'Uganda'}, {code: 'UA', name: 'Ukraine'}, {code: 'AE', name: 'United Arab Emirates'}, {code: 'GB', name: 'United Kingdom'}, {code: 'US', name: 'United States'}, {code: 'UY', name: 'Uruguay'}, {code: 'UZ', name: 'Uzbekistan'}, {code: 'VU', name: 'Vanuatu'}, {code: 'VA', name: 'Vatican City'}, {code: 'VE', name: 'Venezuela'}, {code: 'VN', name: 'Vietnam'}, {code: 'YE', name: 'Yemen'}, {code: 'ZM', name: 'Zambia'}, {code: 'ZW', name: 'Zimbabwe'},
     // Added Hong Kong and Macao entries
@@ -307,10 +306,10 @@ function getTouristVisaResult(nationality, countryName) {
     let visaType, visaClass;
     
     if (authorizedVisaCountries.includes(nationality)) {
-        visaType = 'AUTHORIZED VISA';
+        visaType = 'AUTHORIZED TOURIST VISA';
         visaClass = 'authorized';
     } else if (stampedVisaCountries.includes(nationality)) {
-        visaType = 'STAMPED VISA';
+        visaType = 'STAMPED TOURIST VISA';
         visaClass = 'stamped';
     } else {
         visaType = 'TOURIST VISA';
@@ -330,51 +329,69 @@ function getTouristVisaResult(nationality, countryName) {
     };
 }
 
-// Get result for transit visas
+// Get result for transit
 function getTransitResult(nationality, countryName) {
-    if (transitExemptionCountries.includes(nationality)) {
+    if (transitRequirementCountries.includes(nationality)) {
         return {
-            class: 'transit-allowed',
+            class: 'transit-exemptions',
             message: `
-                <h3>Transit Allowed Without Visa</h3>
-                <div class="transit-exemptions">
-                    <h4>Transit Exemption Conditions</h4>
-                    <p>As a national of ${countryName} transiting for 12 hours or less, you are allowed to transit without a visa if you meet the following conditions:</p>
-                    <ul class="conditions-list">
-                        <li>You have a confirmed onward ticket</li>
-                        <li>You remain in the international transit area</li>
-                        <li>Your transit time is 12 hours or less</li>
-                    </ul>
+                <h3>Transit Requirements</h3>
+                <div class="visa-type transit">TRANSIT REQUIREMENTS APPLY</div>
+                <p>As a national of ${countryName}, you must meet at least one of the transit requirements to transit through Panama for 12 hours or less.</p>
+                <div class="action-buttons">
+                    <a href="transit-visa" class="requirements-btn">View Transit Requirements</a>
                 </div>
             `
         };
     } else {
-        return {
-            class: 'visa-required',
-            message: `
-                <h3>Transit Visa Required</h3>
-                <div class="visa-type transit">TRANSIT VISA REQUIRED</div>
-                <p>As a national of ${countryName} planning a transit stay of 12 hours or less, you need a transit visa.</p>
-                <div class="action-buttons">
-                    <a href="transit-visa" class="requirements-btn">View Required Documents</a>
-                </div>
-            `
-        };
+        // Not on transit requirement list - they need a tourist visa for transit
+        return getTouristVisaForTransit(nationality, countryName);
     }
 }
 
-// Display the result
-function displayResult(result) {
-    const resultElement = document.getElementById('result');
-    resultElement.className = `result visible ${result.class}`;
-    resultElement.innerHTML = result.message;
+// Get result for countries that need tourist visa for transit
+function getTouristVisaForTransit(nationality, countryName) {
+    let visaType, visaClass;
     
-    // Scroll to result
-    resultElement.scrollIntoView({ behavior: 'smooth' });
+    if (authorizedVisaCountries.includes(nationality)) {
+        visaType = 'AUTHORIZED TOURIST VISA';
+        visaClass = 'authorized';
+    } else if (stampedVisaCountries.includes(nationality)) {
+        visaType = 'STAMPED TOURIST VISA';
+        visaClass = 'stamped';
+    } else {
+        visaType = 'TOURIST VISA';
+        visaClass = 'transit';
+    }
+    
+    return {
+        class: 'visa-required',
+        message: `
+            <h3>Tourist Visa Required for Transit</h3>
+            <div class="visa-type ${visaClass}">${visaType} REQUIRED</div>
+            <p>As a national of ${countryName} planning to transit through Panama for 12 hours or less, you need a tourist visa.</p>
+            <div class="action-buttons">
+                <a href="tourist-visa" class="requirements-btn">View Required Documents</a>
+            </div>
+        `
+    };
 }
 
-// Show privileged visa result immediately when user selects "yes"
+// Display result for privileged visa holders (immediate response)
 function showPrivilegedVisaResult() {
     const result = getPrivilegedVisaResult();
     displayResult(result);
+    
+    // Hide question 3 and submit button
+    document.getElementById('question3').classList.add('hidden');
+    document.getElementById('submitBtn').classList.remove('visible');
+}
+
+// Display result in the UI
+function displayResult(result) {
+    const resultElement = document.getElementById('result');
+    if (resultElement) {
+        resultElement.className = `result ${result.class} visible`;
+        resultElement.innerHTML = result.message;
+    }
 }
